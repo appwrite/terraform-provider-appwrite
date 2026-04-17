c27acf705208e2a7847bbba469a16a68afdd7265
Levi van Noort <73097785+levivannoort@users.noreply.github.com>
2026-04-17 16:42:17 +0200

    chore: remove duplicate example files from the repository

--- examples/backup.tf
+++
@@ -1,7 +0,0 @@
-resource "appwrite_backup_policy" "daily" {
-  name        = "Daily Backup"
-  services    = ["databases"]
-  resource_id = appwrite_tablesdb.main.id
-  retention   = 7
-  schedule    = "0 2 * * *"
-}
--- examples/data.tf
+++
@@ -1,3 +0,0 @@
-data "appwrite_tablesdb" "main" {
-  id = appwrite_tablesdb.main.id
-}
